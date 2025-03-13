2913246243a66a51b442366d8b2d5426ec203fb0
Hooriya M. Fareed <parih3080@gmail.com>
2025-03-13 17:33:35 +0500

    push_changes

--- database/db_connection.py
+++ database/db_connection.py
@@ -9,12 +9,27 @@
 #         auth_plugin='mysql_native_password'
 #     )
 
+# import mysql.connector
+
+# # 📌 Connect to MySQL Database
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",  # Update as per your credentials
+#         password="guujarMahnoor@123",
+#         database="library_manager_db",
+#         auth_plugin='mysql_native_password'
+#     )
+
+
+
+
 import mysql.connector
 
 # 📌 Connect to MySQL Database
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="https://personal-library-manager2-by-hooriya.streamlit.app/",
         user="root",  # Update as per your credentials
         password="guujarMahnoor@123",
         database="library_manager_db",
